a527b48140dd51376f108b770391716a01204361
yoogchu <echu31@gatech.edu>
2018-02-03 20:32:51 -0500

    button functionality added

--- gVision.py
+++ gVision.py
@@ -2,26 +2,58 @@ import picamera, io
 from google.cloud import vision
 from google.cloud.vision import types
 
-filename = 'now.jpg'
-def detect_labels(path):
-    """Detects labels in the file."""
-    client = vision.ImageAnnotatorClient()
+def detect_labels(path, doLabel=True, doLogo=False, doText=False, doSafe=True):
+	"""Detects labels in the file."""
+	client = vision.ImageAnnotatorClient()
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-        print type(content)
+	with io.open(path, 'rb') as image_file:
+		content = image_file.read()
  
-    image = types.Image(content=content)
+	image = types.Image(content=content)
 
-    response = client.label_detection(image=image)
-    print response
-    labels = response.label_annotations
-    print('Labels:')
+# LABELS
+	if doLabel:
+		response = client.label_detection(image=image)
+		print response
+		labels = response.label_annotations
+		print('Labels:')
 
-    for label in labels:
-        print(label.description)
+		for label in labels:
+			print(label.description)
+# LOGOS
+	if doLogo:
+		response = client.logo_detection(image=image)
+		logos = response.logo_annotations
+		print('Logos:')
 
-camera = picamera.PiCamera()
-camera.capture(filename)
-detect_labels('/home/raspi/Desktop/'+filename)
+		for logo in logos:
+			print(logo.description)
 
+# TEXTS
+	if doText:
+		response = client.text_detection(image=image)
+		texts = response.text_annotations
+		print('Texts:')
+
+		for text in texts:
+			print('\n"{}"'.format(text.description))
+
+# SAFE SEARCH
+	if doSafe:    
+		response = client.safe_search_detection(image=image)
+		safe = response.safe_search_annotation
+
+		# Names of likelihood from google.cloud.vision.enums
+		likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+						   'LIKELY', 'VERY_LIKELY')
+		print('Safe search:')
+
+		print('adult: {}'.format(likelihood_name[safe.adult]))
+		print('medical: {}'.format(likelihood_name[safe.medical]))
+		print('spoofed: {}'.format(likelihood_name[safe.spoof]))
+		print('violence: {}'.format(likelihood_name[safe.violence]))
+
+def main(filename):
+	detect_labels('/home/raspi/Desktop/'+filename)
+if __name__ == '__main__':
+	main()
---
+++ master.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import gVision
+import time
+import picamera
+
+def main():
+	camera = picamera.PiCamera()
+	filename='now.jpg'
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
+	i = 0
+	try:
+		while True:
+			i=i+1
+#			print('Sleeping...'+str(i))
+#			time.sleep(2)
+			button_state = GPIO.input(23)
+			if not button_state:
+				print 'button pressed!'
+				camera.capture(filename)
+				gVision.main(filename)
+	except Exception as e: 
+		print e
+		GPIO.cleanup()
+
+if __name__ == '__main__':
+	main()
